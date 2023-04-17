b3566fb45c68d06ee365b5edb6722ab98d590e1e
Sarves1911 <118594430+Sarves1911@users.noreply.github.com>
2023-04-17 18:11:39 +0530

    Add files via upload

---
+++ linearqueue.c
@@ -0,0 +1,74 @@
+//Array Implementation of queues
+
+#include<stdio.h>
+#define MAX 100
+int front= -1;
+int rear =-1;
+int A[MAX];
+
+void enqueue(int x)
+{
+    if (rear==99)
+    {
+        printf("Queue is full \n");
+        return ;
+    }
+    else if (front==-1 && rear==-1)
+    {
+        front=rear=0;
+        A[rear]=x;
+
+    }
+    else
+    {
+        rear=rear+1;
+        A[rear]=x;
+    }
+}
+
+int isempty()
+{
+    if (front==-1 && rear==-1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+void dequeue()
+{
+    if (front == -1 && rear == -1)
+    {
+        printf("Queue is empty \n");
+        return ;
+    }
+    else if (front == rear) 
+    {
+        front = rear = -1;
+    }
+    else
+    {
+        front = front + 1;
+    }
+}
+
+int main()
+{
+    enqueue(4);
+    dequeue();
+    enqueue(6);
+    enqueue(9);
+    enqueue(6);
+    enqueue(9);
+    enqueue(5);
+    dequeue();
+
+   for (int i =front; i <=rear; i++)
+    {
+        printf("%d \n", A[i]);
+    }
+    return 0;
+}
