bf45dbef29f719e789f076f7a5f2c0c140e94642
Sarves1911 <118594430+Sarves1911@users.noreply.github.com>
2023-04-29 16:53:53 +0530

    Add files via upload

---
+++ search_bst.c
@@ -0,0 +1,76 @@
+// to check the binary tree is a binary search tree or not
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+struct node *create_node(int data)
+{
+    struct node *newnode = (struct node *)malloc(sizeof(struct node));
+    newnode->data = data;
+    newnode->left = newnode->right = NULL;
+    return newnode;
+}
+
+struct node *search(struct node *root, int key)
+{
+    if (root == NULL)
+    {
+        return NULL;
+    }
+    else if (key == root->data)
+    {
+        return root;
+    }
+    else if (key > root->data)
+    {
+        return search(root->right, key);
+    }
+    else
+    {
+        return search(root->left, key);
+    }
+}
+
+int main()
+{
+
+    struct node *root = create_node(5);
+    struct node *p1 = create_node(3);
+    struct node *p2 = create_node(6);
+    struct node *p3 = create_node(1);
+    struct node *p4 = create_node(4);
+    root->left = p1;
+    root->right = p2;
+    p1->left = p3;
+    p1->right = p4;
+
+    //          5
+    //         /  \
+    //        3    6
+    //      /  \
+    //     1     4
+
+    // 5-3-6-1-4
+    int key;
+    printf("Enter element to search \n");
+    scanf("%d", &key);
+
+    struct node *result = search(root, key);
+    if (result != NULL)
+    {
+        printf("%d is present in the tree \n ", result->data);
+    }
+    else
+    {
+        printf("Given element is not present in the tree \n");
+    }
+
+    return 0;
+}
